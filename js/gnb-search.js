866c9875515719b1a72a66f060cbf5f2d607f672
sorayeon <jihwan77@gmail.com>
2022-09-03 02:25:57 +0900

    GNB Search history -  최근 검색어 전체 삭제 기능구현

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -2,7 +2,19 @@ const gnbSearch = document.querySelector('.gnb-search');
 const gnbSearchInput = gnbSearch.querySelector('input');
 const gnbSearchHistory = gnbSearch.querySelector('.search-history');
 
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button',
+);
+const gnbSearchHistoryList = gnbSearchHistory.querySelector(
+  '.search-history-list',
+);
+
 function openGnbSearchHistory() {
+  // 체크 => gnbSearchHistoryList 안에 li가 0개 => 실행 X
+  if (gnbSearchHistoryList.children.length == 0) {
+    return;
+  }
+
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside);
   }
@@ -17,3 +29,11 @@ function closeGnbSearchHistoryOnClickingOutside(e) {
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory);
+
+function deleteAllSearchHistoryItems() {
+  // gnbSearchHistoryList 안에 들어있는 모든 li를 삭제해!
+  gnbSearchHistoryList.innerHTML = '';
+  gnbSearchHistory.classList.remove('is-active');
+}
+
+deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems);
