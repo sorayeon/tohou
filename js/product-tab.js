c8d7ff59ce435deb9d384d4e859274a75f74aed6
sorayeon <jihwan77@gmail.com>
2022-09-04 16:45:19 +0900

    Product Tab - Scroll To TabPannel (getBoundingClientRect)
    * window.scrollBy() : 스크롤이동
    * getBoundingClientRect : element 위치
    https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
    * window.innerWidth : 현재 브라우저 가로 너비

--- js/product-tab.js
+++ js/product-tab.js
@@ -4,10 +4,15 @@
     '.product-tab-item button',
   );
 
+  // 현재 선택된 Tab State
   let currentActiveTab = productTab.querySelector(
     '.product-tab-item.is-active',
   );
 
+  const TOP_HEADER_DESKTOP = 80 + 50 + 54;
+  const TOP_HEADER_MOBILE = 50 + 40 + 40;
+
+  // Product Tab Active 상태 변경
   function activeProductTab() {
     // 1. is-active
     const tabItem = this.parentNode;
@@ -18,7 +23,25 @@
     }
   }
 
+  // 선택된 Tab Panel 로 스크롤 => window.scrollBy({top:x, left:y})
+  function scrollToTabPanel() {
+    const tabPanelId = this.parentNode.getAttribute('aria-labelledby');
+    const tabPanel = document.querySelector(`#${tabPanelId}`);
+
+    // ?? 얼마나 스크롤을 시켜야 하는가 => element.getBoundingClientRect()
+    // window.innerWidth 현재 창의 가로길이
+    const scrollAmount =
+      tabPanel.getBoundingClientRect().top -
+      (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE);
+
+    window.scrollBy({
+      top: scrollAmount,
+      behavior: 'smooth', // 부드럽게 이동
+    });
+  }
+
   productTabButtonList.forEach((button) => {
     button.addEventListener('click', activeProductTab);
+    button.addEventListener('click', scrollToTabPanel);
   });
 })();
