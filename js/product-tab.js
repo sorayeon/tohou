e54cff95da8e6045a17f5d0bce208c082c098d85
sorayeon <jihwan77@gmail.com>
2022-09-04 19:48:43 +0900

    Product Tab - TabPanel position detect

--- js/product-tab.js
+++ js/product-tab.js
@@ -29,7 +29,7 @@
     const tabPanel = document.querySelector(`#${tabPanelId}`);
 
     // ?? 얼마나 스크롤을 시켜야 하는가 => element.getBoundingClientRect()
-    // window.innerWidth 현재 창의 가로길이
+    // window.innerWidth 현재 창의 가로너비
     const scrollAmount =
       tabPanel.getBoundingClientRect().top -
       (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE);
@@ -44,4 +44,41 @@
     button.addEventListener('click', activeProductTab);
     button.addEventListener('click', scrollToTabPanel);
   });
+
+  // 사전정보 : 각 tabPanel의 y축 위치 (문서의 시작점에서 얼마나 아래에 있는지)
+  // 찾는 요소의 Y축 위치 값 = window.scrollY + element.getBoundingClientRect().top
+  const productTabPanelIdList = [
+    'product-spec',
+    'product-review',
+    'product-inquiry',
+    'product-shipment',
+    'product-recommendation',
+  ];
+  const productTabPanelList = productTabPanelIdList.map((tabPanelId) =>
+    document.querySelector(`#${tabPanelId}`),
+  );
+  const productTabPanelPositionMap = {};
+
+  function detectTabPanelPosition() {
+    // 각각의 tabPanel의 y축 위치를 찾는다
+    // productTabPanelPositionMap에 그 값을 업데이트
+    // {
+    // product-spec: 1000,
+    // product-review: 5000,
+    // product-inquiry: 7000,
+    // product-shipment: 8000,
+    // product-recommendation: 10000
+    // }
+    productTabPanelList.forEach((panel) => {
+      // id
+      const id = panel.getAttribute('id');
+      // y축 위치
+      const position = window.scrollY + panel.getBoundingClientRect().top;
+      productTabPanelPositionMap[id] = position;
+    });
+    console.log(productTabPanelPositionMap['product-spec']);
+  }
+
+  window.addEventListener('load', detectTabPanelPosition);
+  window.addEventListener('resize', detectTabPanelPosition);
 })();
