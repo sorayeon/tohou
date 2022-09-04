1746100a931fe2aa1d78a5ea07b2b0f48d1f2ec4
sorayeon <jihwan77@gmail.com>
2022-09-04 14:45:50 +0900

    Product Tab - Active Tab 토글기능 구현

---
+++ js/product-tab.js
@@ -0,0 +1,19 @@
+const productTab = document.querySelector('.product-tab');
+const productTabButtonList = productTab.querySelectorAll(
+  '.product-tab-item button',
+);
+
+let currentActiveTab = productTab.querySelector('.product-tab-item.is-active');
+
+function activeProductTab() {
+  // 1. is-active
+  const tabItem = this.parentNode;
+  if (currentActiveTab !== tabItem) {
+    tabItem.classList.add('is-active');
+    currentActiveTab.classList.remove('is-active');
+    currentActiveTab = tabItem;
+  }
+}
+productTabButtonList.forEach((button) => {
+  button.addEventListener('click', activeProductTab);
+});
