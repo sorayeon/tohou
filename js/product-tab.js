9ebffcd6d2ae950c14ae9c08d767982984c7a901
sorayeon <jihwan77@gmail.com>
2022-09-04 23:17:49 +0900

    Product Tab - Throttle 로 성능개선 (resize, scroll)

--- js/product-tab.js
+++ js/product-tab.js
@@ -140,6 +140,6 @@
   }
 
   window.addEventListener('load', detectTabPanelPosition);
-  window.addEventListener('resize', detectTabPanelPosition);
-  window.addEventListener('scroll', updateActiveTabOnScroll);
+  window.addEventListener('resize', _.throttle(detectTabPanelPosition, 1000));
+  window.addEventListener('scroll', _.throttle(updateActiveTabOnScroll, 300));
 })();
